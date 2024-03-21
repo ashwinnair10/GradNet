a2bdf4c3cfd38cfa434abf48cb207a86a9bced1c
ashwinnair10 <ashwinnair1894@gmail.com>
2024-03-21 19:43:14 +0530

    mod

--- lib/memories.dart
+++ lib/memories.dart
@@ -1,9 +1,13 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_constructors_in_immutables, prefer_final_fields, avoid_print, avoid_unnecessary_containers
+// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, prefer_final_fields, prefer_const_constructors_in_immutables, avoid_print
+
+import 'dart:io';
 
 import 'package:alumniyearbook/alumniprofile.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class Memories extends StatefulWidget {
   Memories({Key? key}) : super(key: key);
@@ -14,26 +18,55 @@ class Memories extends StatefulWidget {
 
 class _MemoriesState extends State<Memories> {
   TextEditingController _contentController = TextEditingController();
+  File? _image;
 
   Future<void> _uploadMemory() async {
     try {
       String content = _contentController.text.trim();
       String? userId = FirebaseAuth.instance.currentUser?.email;
       if (userId != null) {
-        // Add a new document to the 'memories' subcollection
-        await FirebaseFirestore.instance
-            .collection('USERS')
-            .doc(userId)
-            .collection('memories')
-            .add({
-          'content': content,
-          'timestamp': FieldValue.serverTimestamp(),
-          'url': '',
-        });
+        String url = '';
+        if (_image != null) {
+          Reference ref = FirebaseStorage.instance
+              .ref()
+              .child('memories')
+              .child(DateTime.now().millisecondsSinceEpoch.toString());
+          await ref.putFile(_image!);
+          url = await ref.getDownloadURL();
+        }
+        if (_contentController.text != '') {
+          await FirebaseFirestore.instance
+              .collection('USERS')
+              .doc(userId)
+              .collection('memories')
+              .add({
+            'content': content,
+            'timestamp': FieldValue.serverTimestamp(),
+            'url': url,
+          });
+        }
       }
       _contentController.clear();
+      setState(() {
+        _image = null;
+      });
     } catch (e) {
       print("Error uploading memory: $e");
+    }
+  }
+
+  Future<void> _getImage() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      type: FileType.image,
+      allowMultiple: false,
+    );
+
+    if (result != null) {
+      setState(() {
+        _image = File(result.files.single.path!);
+      });
+    } else {
+      // User canceled the picker
     }
   }
 
@@ -115,15 +148,15 @@ class _MemoriesState extends State<Memories> {
                                                   fontWeight: FontWeight.w500,
                                                 ),
                                               ),
-                                              SizedBox(
-                                                height: 10,
-                                              ),
-                                              if (memory['url'] != '')
+                                              SizedBox(height: 5),
+                                              if (memory['url'] != null &&
+                                                  memory['url'] != '')
                                                 ClipRRect(
                                                   borderRadius:
                                                       BorderRadius.circular(10),
                                                   child: Image.network(
                                                     memory['url'],
+                                                    fit: BoxFit.cover,
                                                   ),
                                                 ),
                                             ],
@@ -131,8 +164,9 @@ class _MemoriesState extends State<Memories> {
                                         ),
                                         title: TextButton(
                                           style: TextButton.styleFrom(
-                                              padding: EdgeInsets.all(0),
-                                              alignment: Alignment.centerLeft),
+                                            padding: EdgeInsets.all(0),
+                                            alignment: Alignment.centerLeft,
+                                          ),
                                           child: Text(
                                             '${userDoc['name']}',
                                             textAlign: TextAlign.left,
@@ -192,17 +226,26 @@ class _MemoriesState extends State<Memories> {
                 ),
                 hintText: 'Enter your memories...',
                 labelText: 'Enter your memories...',
+                suffixIcon: IconButton(
+                  onPressed: _getImage,
+                  icon: Icon(Icons.attach_file),
+                ),
               ),
             ),
           ),
-          TextButton(
-            onPressed: _uploadMemory,
-            child: Text(
-              'Post Memory',
-              style: TextStyle(
-                color: Color.fromARGB(255, 1, 62, 133),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              TextButton(
+                onPressed: _uploadMemory,
+                child: Text(
+                  'Post Memory',
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 1, 62, 133),
+                  ),
+                ),
               ),
-            ),
+            ],
           ),
           SizedBox(
             height: 10,
